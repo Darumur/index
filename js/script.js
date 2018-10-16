4a56ad62e88bb7a85693dd9cfb2187247b3197d4
Darumur <42714520+Darumur@users.noreply.github.com>
2018-10-16 21:53:43 +0300

    edit-task9

--- js/script.js
+++ js/script.js
@@ -12,31 +12,24 @@ function checkEvent(event) {
  var email = document.getElementById('email');
  var valueEmail = email.value;
 
- if (valueName, valueEmail != ""){
+ if (valueName !== "" && valueEmail != ""){
  	name.style.display = "none"; 
  	email.style.display = "none"; 
  	button.style.display = "none"; 
  	var message = document.getElementById('message');
  	message.style.display = "block";
-
  }
  if (valueName === ""){
  	name.style.border = "1px solid red"; 
- 	console.log("Введите имя!");
  }
  else {
  	name.style.border = "1px solid gray"; 
+	email.style.border = "1px solid gray"; 
  }
  if (valueEmail === ""){
  	email.style.border = "1px solid red"; 
- 	console.log("Введите email!");
+	
  }
- else {
- 	email.style.border = "1px solid gray"; 
- }
-
- console.log(valueName);
- console.log("Готово");
 }
 
 
