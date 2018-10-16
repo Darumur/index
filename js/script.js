1939dea6237fa6c299b8f37cc3f92ff6a5ba4cb3
Darumur <42714520+Darumur@users.noreply.github.com>
2018-10-17 01:26:11 +0300

    task9

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 
 var mainButton = document.getElementById('button');
+var i = 0;
 
-console.log(mainButton);
+function checkFields() {
+ 
+ var name = document.getElementById('name');
+ var email = document.getElementById('email');
+ var valueName = name.value;
+ var valueEmail = email.value;
+
+	if ( valueName.length != 0 && valueEmail.length != 0 && i == 0 ) {
+		mainButton.classList.toggle("active");
+		i = 1;
+	}
+}
 
 function checkEvent(event) {
  event.preventDefault();
  
  var name = document.getElementById('name');
- var valueName = name.value;
-
  var email = document.getElementById('email');
+ var valueName = name.value;
  var valueEmail = email.value;
-
- if (valueName !== "" && valueEmail != ""){
+ 
+ if (valueName !== "" && valueEmail != "" && mainButton.classList.contains('active')){
  	name.style.display = "none"; 
  	email.style.display = "none"; 
  	button.style.display = "none"; 
@@ -28,7 +39,6 @@ function checkEvent(event) {
  }
  if (valueEmail === ""){
  	email.style.border = "1px solid red"; 
-	
  }
 }
 
